6466a2b98b90e39d89c74d498fa0044959751bd7
zhoujijian <394463337@qq.com>
2017-03-04 09:56:35 +0800

    mailbox for actor

---
+++ Core/ActorMailbox.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Core {
+	public class ActorMailbox {
+		private ActorContext context;
+		private Queue<ActorMessage> msgQ = new Queue<ActorMessage>();
+
+		public ActorMailbox(ActorContext context) {
+			this.context = context;
+		}
+
+		public void Post(ActorMessage msg) {
+			lock(msgQ) {
+				msgQ.Enqueue(msg);
+				if (msgQ.Count > 1) { return; }
+			}
+			runTask(msg);
+		}
+
+		private void runTask(ActorMessage msg) {
+			Task.Run(async () => {
+				object ret = await context.RecvCallAsync(msg);
+				if (msg.Kind == ActorMessage.REQ) {
+					ActorMessage reply = new ActorMessage(ActorMessage.REP, msg.Session, context.ID, msg.Source, null, ret);
+					context.System.Send(reply);
+				}
+
+				ActorMessage another = null;
+				lock(msgQ) {
+					msgQ.Dequeue();
+					if (msgQ.Count > 0) {
+						another = msgQ.Peek();
+					}
+				}
+				if (another != null) {
+					runTask(another);
+				}
+			});
+		}
+	}
+}

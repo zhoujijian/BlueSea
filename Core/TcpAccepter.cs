bb76020253e52be3e45d32bd4d2817866e2b873c
zhoujijian <394463337@qq.com>
2018-11-06 19:23:53 +0800

    TcpAccepter, TcpChannel

---
+++ Core/TcpAccepter.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Core {
+    public class TcpAccepter {
+        private int port;
+
+        public TcpAccepter(int port) {
+            this.port = port;
+        }
+
+        public async void Start() {
+            var server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            
+            while(true) {
+                var client = await server.AcceptTcpClientAsync();
+                var channel = new TcpChannel(client);
+                await Task.Run(() => channel.Run());
+            }
+        }
+    }
+}
---
+++ Core/TcpChannel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Core {
+    internal class TcpChannel {
+        private TcpClient client;
+        private byte[] buffer = new byte[1024];
+
+        public TcpChannel(TcpClient client) {
+            this.client = client;
+        }
+
+        public async void Run() {
+            var stream = client.GetStream();
+            while(true) {
+                var read = await stream.ReadAsync(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
